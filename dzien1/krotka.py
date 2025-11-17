2bef742c2c3adbc5b675d0f404185ee0e06bd7f4
albim72 <marcin.albiniak72@gmail.com>
2025-11-17 14:04:25 +0100

    Create krotka.py

---
+++ dzien1/krotka.py
@@ -0,0 +1,13 @@
+#tworzenie krotki
+#krotka - tuple - immutable sequence
+person=("Marcin",52,"AI Architect")
+print(person)
+
+im = person.index("Marcin")
+print(im)
+#tuple unpacking
+name,age,role = person
+
+print(name)
+print(age)
+print(role)
