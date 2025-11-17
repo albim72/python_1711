11e05119edd287da80c72a889e34ee9af2dea66e
albim72 <marcin.albiniak72@gmail.com>
2025-11-17 14:18:33 +0100

    Create zbiory.py

---
+++ dzien1/zbiory.py
@@ -0,0 +1,26 @@
+a = {'d','r','e','h','x','l','e'}
+print(a)
+print(type(a))
+
+print(a)
+
+a.add('b')
+a.add('y')
+
+print(a)
+
+# print(a[4])
+a.add('e')
+print(a)
+
+e = list(a)
+
+e.sort()
+print(e)
+
+nb = [4,2,5,2,6,3,7,7,3,2,2,4,5,7,7,4,33,4,6,3]
+
+nb = list(set(nb))
+print(nb)
+
+
